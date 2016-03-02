f91747e4413ea94b28be70e7c980011af0ef99a9
Alex <alexyan981@gmail.com>
2016-03-01 20:29:54 -0500

    Greatly improved tokenizer
    Replaced parts that were cobbled together
    Changed tokens. Added a constant token to help preprocess defines
    Moved all constant declaration related to the language into global space (opcodes, token definitions etc)
    Shortened some names

--- Assembler6502.py
+++ Assembler6502.py
@@ -1,10 +1,61 @@
 import re
 
-class Token:
-	def __init__(self, tokenType):
-		self.__init__(tokenType, None)
+DEF, ID, INS, CONST, COL, HASH, CMA, LB, RB, IREG, COMMENT, NEWLINE, BLANK = 'DEFINE', 'IDENTIFIER', 'INSTRUCTION', 'CONSTANT', 'COLON', 'HASH', 'COMMA', 'LEFT BRACKET', 'RIGHT BRACKET', 'INDEX REGISTER', 'COMMENT', 'NL', 'BLANK'
+skip = {COMMENT, BLANK}
+priorities = [DEF, INS, ID, COMMENT, NEWLINE, BLANK, IREG, COL, RB, LB, CMA, HASH, CONST]
 
-	def __init__(self, tokenType, value):
+tokens = {
+	DEF    : r'\bdefine\b',
+	ID     : r'\b([a-zA-z_]\w+)\b',
+	INS    : r'\b([a-zA-Z]{3})\b',
+	CONST  : r'\b([0-9a-fA-F]+)\b|(\$[0-9a-fA-F]+)\b|(#\$[0-9a-fA-F]+)\b',
+	COL    : r':',
+	HASH   : r'#',
+	CMA    : r',',
+	LB     : r'\(',
+	RB     : r'\)',
+	IREG   : r'\b([xXyY])\b',
+	COMMENT: r';.+',
+	NEWLINE: r'\r\n|\r|\n',
+	BLANK  : r'\s+'
+}
+
+valuables = {ID, INS, CONST, IREG}
+
+noOp = 'No operand'
+imd = 'Immediate'
+absl = 'Absolute'
+rel = 'Relative'
+ind = 'Indirect'
+zpg = 'Zero Page'
+zpx = 'Zero Page X Indexed'
+zpy = 'Zero Page Y Indexed'
+xInd = 'X Indirected'
+indY = 'Indirected Y'
+absX = 'Absolute X Indexed'
+absY = 'Absolute Y Indexed'
+
+opcodes = {
+	('brk', noOp)	: 0x00,	('ora', xInd): 0x01,                                         ('ora', zpg): 0x05, ('asl', zpg): 0x06, ('php', noOp): 0x08, ('ora', imd)	: 0x09, ('asl', noOp): 0x0A,                        ('ora', absl): 0x0D, ('asl', absl): 0x0E,
+	('bpl', rel)	: 0x10,	('ora', indY): 0x11,                                         ('ora', zpx): 0x15, ('asl', zpx): 0x16, ('clc', noOp): 0x18, ('ora', absY)	: 0x19,                                             ('ora', absX): 0x1D, ('asl', absX): 0x1E,
+	('jsr', absl)	: 0x20,	('and', xInd): 0x21,                     ('bit', zpg): 0x24, ('and', zpg): 0x25, ('rol', zpg): 0x26, ('plp', noOp): 0x28, ('and', imd)	: 0x29, ('rol', noOp): 0x2A, ('bit', absl)	: 0x2C, ('and', absl): 0x2D, ('rol', absl): 0x2E,
+	('bmi', rel)	: 0x30,	('and', indY): 0x31,                                         ('and', zpx): 0x35, ('rol', zpx): 0x36, ('sec', noOp): 0x38, ('and', absY)	: 0x39,                                             ('and', absX): 0x3D, ('rol', absX): 0x3E,
+	('rti', noOp)	: 0x40,	('eor', xInd): 0x41,                                         ('eor', zpg): 0x45, ('lsr', zpg): 0x46, ('pha', noOp): 0x48, ('eor', imd)	: 0x49, ('lsr', noOp): 0x4A, ('jmp', absl)	: 0x4C, ('eor', absl): 0x4D, ('lsr', absl): 0x4E,
+	('bvc', rel)	: 0x50,	('eor', indY): 0x51,                                         ('eor', zpx): 0x55, ('lsr', zpx): 0x56, ('cli', noOp): 0x58, ('eor', absY)	: 0x59,                                             ('eor', absX): 0x5D, ('lsr', absX): 0x5E,
+	('rts', noOp)	: 0x60,	('adc', xInd): 0x61,                                         ('adc', zpg): 0x65, ('ror', zpg): 0x66, ('pla', noOp): 0x68, ('adc', imd)	: 0x69, ('ror', noOp): 0x6A, ('jmp', absl)	: 0x6C, ('adc', absl): 0x6D, ('ror', absl): 0x6E,
+	('bvs', rel)	: 0x70,	('adc', indY): 0x71,                                         ('adc', zpx): 0x75, ('ror', zpx): 0x76, ('sei', noOp): 0x78, ('adc', absY)	: 0x79,                                             ('adc', absX): 0x7D, ('ror', absX): 0x7E,
+	                        ('sta', xInd): 0x81,                     ('sty', zpg): 0x84, ('sta', zpg): 0x85, ('stx', zpg): 0x86, ('dey', noOp): 0x88,                       ('txa', noOp): 0x8A, ('sty', absl)	: 0x8C, ('sta', absl): 0x8D, ('stx', absl): 0x8E,
+	('bcc', rel)	: 0x90,	('sta', indY): 0x91,                     ('sty', zpx): 0x94, ('sta', zpx): 0x95, ('stx', zpx): 0x96, ('tya', noOp): 0x98, ('sta', absY)	: 0x99, ('txs', noOp): 0x9A,                        ('sta', absX): 0x9D,
+	('ldy', imd)	: 0xA0,	('lda', xInd): 0xA1, ('ldx', imd): 0xA2, ('ldy', zpg): 0xA4, ('lda', zpg): 0xA5, ('ldx', zpg): 0xA6, ('tay', noOp): 0xA8, ('lda', imd)	: 0xA9, ('tax', noOp): 0xAA, ('ldy', absl)	: 0xAC, ('lda', absl): 0xAD, ('ldx', absl): 0xAE,
+	('bcs', rel)	: 0xB0,	('lda', indY): 0xB1,                     ('ldy', zpx): 0xB4, ('lda', zpx): 0xB5, ('ldx', zpx): 0xB6, ('clv', noOp): 0xB8, ('lda', absY)	: 0xB9, ('tsx', noOp): 0xBA, ('ldy', absX)	: 0xBC, ('lda', absX): 0xBD, ('ldx', absX): 0xBE,
+	('cpy', imd)	: 0xC0,	('cmp', xInd): 0xC1,                     ('cpy', zpg): 0xC4, ('cmp', zpg): 0xC5, ('dec', zpg): 0xC6, ('iny', noOp): 0xC8, ('cmp', imd)	: 0xC9, ('dex', noOp): 0xCA, ('cpy', absl)	: 0xCC, ('cmp', absl): 0xCD, ('dec', absl): 0xCE,
+	('bne', rel)	: 0xD0,	('cmp', indY): 0xD1,                                         ('cmp', zpx): 0xD5, ('dec', zpx): 0xD6, ('cld', noOp): 0xD8, ('cmp', absY)	: 0xD9,                                             ('cmp', absX): 0xDD, ('dec', absX): 0xDE,
+	('cpx', imd)	: 0xE0,	('sbc', xInd): 0xE1,                     ('cpx', zpg): 0xE4, ('sbc', zpg): 0xE5, ('inc', zpg): 0xE6, ('inx', noOp): 0xE8, ('sbc', imd)	: 0xE9, ('nop', noOp): 0xEA, ('cpx', absl)	: 0xEC, ('sbc', absl): 0xED, ('inc', absl): 0xEE,
+	('beq', rel)	: 0xF0,	('sbc', indY): 0xF1,                                         ('sbc', zpx): 0xF5, ('inc', zpx): 0xF6, ('sed', noOp): 0xF8, ('sbc', absY)	: 0xF9,                                             ('sbc', absX): 0xFD, ('inc', absX): 0xFE
+}
+
+class Token:
+	def __init__(self, tokenType, value = None):
 		self.tokenType = tokenType
 		self.value = value
 
@@ -15,127 +66,80 @@ class Token:
 		return '%s: "%s"' % (self.tokenType, self.value)
 
 class Tokenizer:
-	def __init__(self, filename):
-		self.tokens = {
-			'KEY': r'define',
-			'ID' : r'^[^\d\W]+[a-zA-Z0-9]+$',
-			'IST': r'^[a-zA-Z]{3}$',
-			'NBR': r'^\$?[0-9a-fA-F]+$',
-			'COL': r':',
-			'LIT': r'^#$',
-			'CMA': r'^,$',
-			'LB' : r'^\($',
-			'RB' : r'^\)$',
-			'REG': r'[xXyY]'
-		}
+	def __init__(self, source):
+		self.source = source
+		self.patterns = {}
 
-		for key in self.tokens:
-			pattern = self.tokens[key]
-			self.tokens[key] = re.compile(pattern)
+		for token in tokens:
+			pattern = tokens[token]
+			self.patterns[token] = re.compile(pattern)
 
-		self.priorities = ['KEY', 'REG', 'COL', 'RB', 'LB', 'CMA', 'LIT', 'IST', 'NBR', 'ID']
+		self.tokenize()
+		self.sanitize()
 
-		self.file = open(filename, 'r')
-		self.source = self.file.read()
+	def tokenize(self):
+		self.tokenStream = []
 
-		lines = re.split('[\n\r]', self.source)
-		lines = [re.sub(';.+', '', line) for line in lines]
-		lines = [line.strip() for line in lines]
-		lines = [line for line in lines if len(line) > 0]
-		self.lines = [self.tokenize(line) for line in lines]
-		self.lines = [token for token in self.lines if token != None]
+		begin = 0
+		while begin < len(self.source):
+			valid = False
 
-	def tokenize(self, line):
-		splitLine = re.split('\s+|\b', line)
-		tokenStr = []
+			for token in priorities:
+				pattern = tokens[token]
+				matcher = re.compile(pattern)
+				match = matcher.match(self.source, begin)
 
-		for token in splitLine:
-			subtoken = ""
-			for c in token:
-				if "#.,():".find(c) != -1:
-					if len(subtoken) > 0:
-						tokenStr.append(subtoken)
-					subtoken = ""
-					tokenStr.append(c)
-				else:
-					subtoken += c
+				if not match:
+					continue
 
-			if len(subtoken) > 0:
-				tokenStr.append(subtoken)
+				valid = True
+				begin = match.end()
 
-		tokens = []
+				if token in skip:
+					break
 
-		for token in tokenStr:
-			size = len(tokens)
+				tokenObj = Token(token) if token not in valuables else Token(token, match.group())
 
-			for tokenType in self.priorities:
-				pattern = self.tokens[tokenType]
+				self.tokenStream.append(tokenObj)
+				break
 
-				if pattern.match(token):
-					tokens.append(Token(tokenType, token))
-					break;
+			if not valid:
+				end = self.patterns[NEWLINE].search(self.source, begin)
+				raise Exception('Bad token: "%s"' % self.source[begin:end.span()[0]])
 
-			if size == len(tokens):
-				print('"%s" is not a valid token. Line ditched' % token)
+	def sanitize(self):
+		tokenStream = []
 
-				return None;
+		while self.tokenStream[0].tokenType == NEWLINE:
+			self.tokenStream.pop(0)
 
-		return tokens
+		for token in self.tokenStream:
+			if len(tokenStream) == 0:
+				tokenStream.append(token)
+				continue
+
+			if tokenStream[-1].tokenType == NEWLINE and tokenStream[-1].tokenType == token.tokenType:
+				continue
+
+			tokenStream.append(token)
+
+		self.tokenStream = tokenStream
 
 class Parser:
-	def __init__(self, lines):
+	def __init__(self, tokens):
 		self.binary = []
 		self.constants = {}
 		self.labels = {}
 		self.subParsers = {
-			'KEY': self.parseKeyword,
-			'ID' : self.parseID,
-			'IST': self.parseInstruction,
-			'NBR': self.parseHex,
-			'COL': self.parseNone,
-			'LIT': self.parseNone,
-			'HEX': self.parseNone,
-			'CMA': self.parseNone,
-			'LB' : self.parseNone,
-			'RB' : self.parseNone,
-			'REG': self.parseNone
+			DEF: self.parseDefine,
+			ID : self.parseID,
+			INS: self.parseInstruction,
+			CONST: self.parseHex
 		}
 
-		self.noOperand = 'No operand'
-		self.immediate = 'Immediate'
-		self.absolute = 'Absolute'
-		self.relative = 'Relative'
-		self.indirect = 'Indirect'
-		self.zeroPage = 'Zero Page'
-		self.zpxIndex = 'Zero Page X Indexed'
-		self.zpyIndex = 'Zero Page Y Indexed'
-		self.xIndirect = 'X Indirected'
-		self.indirectY = 'Indirected Y'
-		self.absX = 'Absolute X Indexed'
-		self.absY = 'Absolute Y Indexed'
+		self.parse()
 
-		self.opcodes = {
-			('brk', self.noOperand)	: 0x00,	('ora', self.xIndirect): 0x01,                                                              ('ora', self.zeroPage): 0x05, ('asl', self.zeroPage): 0x06, ('php', self.noOperand): 0x08, ('ora', self.immediate): 0x09, ('asl', self.noOperand): 0x0A,                               ('ora', self.absolute): 0x0D, ('asl', self.absolute): 0x0E,
-			('bpl', self.relative)	: 0x10,	('ora', self.indirectY): 0x11,                                                              ('ora', self.zpxIndex): 0x15, ('asl', self.zpxIndex): 0x16, ('clc', self.noOperand): 0x18, ('ora', self.absY)     : 0x19,                                                              ('ora', self.absX)    : 0x1D, ('asl', self.absX)    : 0x1E,
-			('jsr', self.absolute)	: 0x20,	('and', self.xIndirect): 0x21,                                ('bit', self.zeroPage): 0x24, ('and', self.zeroPage): 0x25, ('rol', self.zeroPage): 0x26, ('plp', self.noOperand): 0x28, ('and', self.immediate): 0x29, ('rol', self.noOperand): 0x2A, ('bit', self.absolute): 0x2C, ('and', self.absolute): 0x2D, ('rol', self.absolute): 0x2E,
-			('bmi', self.relative)	: 0x30,	('and', self.indirectY): 0x31,                                                              ('and', self.zpxIndex): 0x35, ('rol', self.zpxIndex): 0x36, ('sec', self.noOperand): 0x38, ('and', self.absY)     : 0x39,                                                              ('and', self.absX)    : 0x3D, ('rol', self.absX)    : 0x3E,
-			('rti', self.noOperand)	: 0x40,	('eor', self.xIndirect): 0x41,                                                              ('eor', self.zeroPage): 0x45, ('lsr', self.zeroPage): 0x46, ('pha', self.noOperand): 0x48, ('eor', self.immediate): 0x49, ('lsr', self.noOperand): 0x4A, ('jmp', self.absolute): 0x4C, ('eor', self.absolute): 0x4D, ('lsr', self.absolute): 0x4E,
-			('bvc', self.relative)	: 0x50,	('eor', self.indirectY): 0x51,                                                              ('eor', self.zpxIndex): 0x55, ('lsr', self.zpxIndex): 0x56, ('cli', self.noOperand): 0x58, ('eor', self.absY)     : 0x59,                                                              ('eor', self.absX)    : 0x5D, ('lsr', self.absX)    : 0x5E,
-			('rts', self.noOperand)	: 0x60,	('adc', self.xIndirect): 0x61,                                                              ('adc', self.zeroPage): 0x65, ('ror', self.zeroPage): 0x66, ('pla', self.noOperand): 0x68, ('adc', self.immediate): 0x69, ('ror', self.noOperand): 0x6A, ('jmp', self.absolute): 0x6C, ('adc', self.absolute): 0x6D, ('ror', self.absolute): 0x6E,
-			('bvs', self.relative)	: 0x70,	('adc', self.indirectY): 0x71,                                                              ('adc', self.zpxIndex): 0x75, ('ror', self.zpxIndex): 0x76, ('sei', self.noOperand): 0x78, ('adc', self.absY)     : 0x79,                                                              ('adc', self.absX)    : 0x7D, ('ror', self.absX)    : 0x7E,
-			                                ('sta', self.xIndirect): 0x81,                                ('sty', self.zeroPage): 0x84, ('sta', self.zeroPage): 0x85, ('stx', self.zeroPage): 0x86, ('dey', self.noOperand): 0x88,                                ('txa', self.noOperand): 0x8A, ('sty', self.absolute): 0x8C, ('sta', self.absolute): 0x8D, ('stx', self.absolute): 0x8E,
-			('bcc', self.relative)	: 0x90,	('sta', self.indirectY): 0x91,                                ('sty', self.zpxIndex): 0x94, ('sta', self.zpxIndex): 0x95, ('stx', self.zpxIndex): 0x96, ('tya', self.noOperand): 0x98, ('sta', self.absY)     : 0x99, ('txs', self.noOperand): 0x9A,                               ('sta', self.absX)    : 0x9D,
-			('ldy', self.immediate)	: 0xA0,	('lda', self.xIndirect): 0xA1, ('ldx', self.immediate): 0xA2, ('ldy', self.zeroPage): 0xA4, ('lda', self.zeroPage): 0xA5, ('ldx', self.zeroPage): 0xA6, ('tay', self.noOperand): 0xA8, ('lda', self.immediate): 0xA9, ('tax', self.noOperand): 0xAA, ('ldy', self.absolute): 0xAC, ('lda', self.absolute): 0xAD, ('ldx', self.absolute): 0xAE,
-			('bcs', self.relative)	: 0xB0,	('lda', self.indirectY): 0xB1,                                ('ldy', self.zpxIndex): 0xB4, ('lda', self.zpxIndex): 0xB5, ('ldx', self.zpxIndex): 0xB6, ('clv', self.noOperand): 0xB8, ('lda', self.absY)     : 0xB9, ('tsx', self.noOperand): 0xBA, ('ldy', self.absX)    : 0xBC, ('lda', self.absX)    : 0xBD, ('ldx', self.absX)    : 0xBE,
-			('cpy', self.immediate)	: 0xC0,	('cmp', self.xIndirect): 0xC1,                                ('cpy', self.zeroPage): 0xC4, ('cmp', self.zeroPage): 0xC5, ('dec', self.zeroPage): 0xC6, ('iny', self.noOperand): 0xC8, ('cmp', self.immediate): 0xC9, ('dex', self.noOperand): 0xCA, ('cpy', self.absolute): 0xCC, ('cmp', self.absolute): 0xCD, ('dec', self.absolute): 0xCE,
-			('bne', self.relative)	: 0xD0,	('cmp', self.indirectY): 0xD1,                                                              ('cmp', self.zpxIndex): 0xD5, ('dec', self.zpxIndex): 0xD6, ('cld', self.noOperand): 0xD8, ('cmp', self.absY)     : 0xD9,                                                              ('cmp', self.absX)    : 0xDD, ('dec', self.absX)    : 0xDE,
-			('cpx', self.immediate)	: 0xE0,	('sbc', self.xIndirect): 0xE1,                                ('cpx', self.zeroPage): 0xE4, ('sbc', self.zeroPage): 0xE5, ('inc', self.zeroPage): 0xE6, ('inx', self.noOperand): 0xE8, ('sbc', self.immediate): 0xE9, ('nop', self.noOperand): 0xEA, ('cpx', self.absolute): 0xEC, ('sbc', self.absolute): 0xED, ('inc', self.absolute): 0xEE,
-			('beq', self.relative)	: 0xF0,	('sbc', self.indirectY): 0xF1,                                                              ('sbc', self.zpxIndex): 0xF5, ('inc', self.zpxIndex): 0xF6, ('sed', self.noOperand): 0xF8, ('sbc', self.absY)     : 0xF9,                                                              ('sbc', self.absX)    : 0xFD, ('inc', self.absX)    : 0xFE
-		}
-
-		self.parseProgram(lines)
-
-	def parseKeyword(self, tokens):
+	def parseDefine(self, tokens):
 		tokens.pop(0)
 
 		if tokens[0].tokenType != 'ID':
@@ -192,7 +196,7 @@ class Parser:
 	def parseOperand(self, tokens):
 		print('  %s' % tokens)
 		if len(tokens) == 0:
-			return (self.noOperand, 0)
+			return (self.noOp, 0)
 
 		stream = []
 
@@ -224,16 +228,16 @@ class Parser:
 			# print('  %s' % stream)
 
 			# if operand.tokenType == 'ID':
-			# 	return (self.immediate, self.constants[operand.value])
-			return (self.immediate, self.parseNumber(operand))
+			# 	return (self.imd, self.constants[operand.value])
+			return (self.imd, self.parseNumber(operand))
 
 		operand = stream[0]
 
 		if operand.tokenType == 'NBR':
 			if len(operand.value) == 3:
-				return (self.relative, self.parseNumber(operand))
+				return (self.rel, self.parseNumber(operand))
 			else:
-				return (self.absolute, self.parseNumber(operand))
+				return (self.absl, self.parseNumber(operand))
 
 		if operand.tokenType == 'LB':
 			print(stream)
@@ -241,12 +245,12 @@ class Parser:
 			operand = stream[0]
 
 			if stream[0] == 'CMA':
-				return (self.xIndirect, self.parseNumber(operand))
+				return (self.xInd, self.parseNumber(operand))
 			elif stream[0] == 'RB':
-				return (self.indirectY, self.parseNumber(operand))
+				return (self.indY, self.parseNumber(operand))
 			else:
 				stream.pop(0)
-				return (self.indirect, self.parseNumber(operand))
+				return (self.ind, self.parseNumber(operand))
 
 	def parseInstruction(self, tokens):
 		print(tokens)
@@ -258,11 +262,11 @@ class Parser:
 		mode = self.parseOperand(tokens)
 		key = (instruction.value, mode[0])
 
-		if not (key in self.opcodes) and mode[0] == self.relative:
-			key = (key[0], self.absolute)
+		if not (key in self.opcodes) and mode[0] == self.rel:
+			key = (key[0], self.absl)
 
-		if not (key in self.opcodes) and mode[0] == self.absolute:
-			key = (key[0], self.relative)
+		if not (key in self.opcodes) and mode[0] == self.absl:
+			key = (key[0], self.rel)
 
 		print(key)
 
@@ -271,20 +275,20 @@ class Parser:
 	def parseHex(self, token):
 		return int(token.value, 16)
 
-	def parseProgram(self, lines):
-		for line in lines:
-			self.parseLine(line)
-
-	def parseNone(self, lines):
+	def parse(self):
 		pass
+		# for line in lines:
+		# 	self.parseLine(line)
 
 	def parseLine(self, line):
 		self.subParsers[line[0].tokenType](line)
 
-tokenizer = Tokenizer('test.asm')
-# print(tokenizer.lines)
+file = open('test.asm')
+source = file.read()
+tokenizer = Tokenizer(source)
+print(tokenizer.tokenStream)
 # print()
-parser = Parser(tokenizer.lines)
+parser = Parser(tokenizer.tokenStream)
 # print(parser.constants)
 # print()
 # print(parser.labels)
